9fd7d84b714e821848eee7c9627b50a8a2ea407a
firepolo <anthony.pasche@gmail.com>
2021-05-15 17:05:18 +0200

    Add route for api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/hello', function(Request $request) {
+    return response()->json('Hello!', 200);
 });
