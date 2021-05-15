0f7ae770d7b9f27a32c4678b44af7baa4af43ee5
firepolo <anthony.pasche@gmail.com>
2021-05-15 17:06:17 +0200

    Add request to api

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import axios from 'axios';
 
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 Vue.use(VueRouter);
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const app = new Vue({
 	el: '#app',
 	mounted() {
+		axios.get('/api/hello').then(response => {
+			console.log(response);
+		}).catch(error => {
+			console.error(error);
+		});
+
 		console.log('MOUNTED');
 	}
 });
