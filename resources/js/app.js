33466ab7cdf9e797e4d81b821e0653170afdae98
firepolo <anthony.pasche@gmail.com>
2021-05-15 12:25:56 +0000

    Add vuejs in project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+	el: '#app',
+	mounted() {
+		console.log('MOUNTED');
+	}
+});
