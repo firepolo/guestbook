686bf3d8b793b41832b861c732b5e12b841f5405
firepolo <anthony.pasche@gmail.com>
2021-05-26 19:51:51 +0200

    add posts for user side

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        return Post::all()->toJson();
+    }
+
+    public function show(Post $post)
+    {
+        return $post->toJson();
+    }
+
     public function create()
     {
     }
